67c9c1d49859154dd38cc7e73a663de116c7efc7
trigun117 <chuvak117@gmail.com>
2019-03-05 01:24:42 +0200

    Changed server response on errors

--- server.go
+++ server.go
@@ -16,56 +16,58 @@ type Response struct {
 const token = ""
 
 func dataProcess(w http.ResponseWriter, r *http.Request) {
-	userFile, header, err := r.FormFile("speech")
-	if err != nil {
-		log.Println(err)
-		w.Write([]byte(`<script>window.location="/";alert('File upload error')</script>`))
-		return
-	}
-	defer userFile.Close()
+	if r.Method == http.MethodPost {
+		userFile, header, err := r.FormFile("speech")
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer userFile.Close()
 
-	fileCreating, err := os.OpenFile("./files/"+header.Filename, os.O_WRONLY|os.O_CREATE, 0644)
-	if err != nil {
-		log.Println(err)
-		w.Write([]byte(`<script>window.location="/";alert('File creating error')</script>`))
-		return
-	}
-	defer os.Remove("./files/" + header.Filename)
-	defer fileCreating.Close()
+		fileCreating, err := os.OpenFile("./files/"+header.Filename, os.O_WRONLY|os.O_CREATE, 0644)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer os.Remove("./files/" + header.Filename)
+		defer fileCreating.Close()
 
-	_, err = io.Copy(fileCreating, userFile)
-	if err != nil {
-		log.Println(err)
-		w.Write([]byte(`<script>window.location="/";alert('File writting error')</script>`))
-		return
-	}
+		_, err = io.Copy(fileCreating, userFile)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 
-	fileForSend, err := os.Open("./files/" + header.Filename)
-	if err != nil {
-		log.Println(err)
-		w.Write([]byte(`<script>window.location="/";alert('File opening error')</script>`))
-		return
-	}
-	defer fileForSend.Close()
+		fileForSend, err := os.Open("./files/" + header.Filename)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer fileForSend.Close()
 
-	client := witai.NewClient(token)
-	msg, err := client.Speech(
-		&witai.MessageRequest{
-			Speech: &witai.Speech{
-				File:        fileForSend,
-				ContentType: "audio/wav",
+		client := witai.NewClient(token)
+		msg, err := client.Speech(
+			&witai.MessageRequest{
+				Speech: &witai.Speech{
+					File:        fileForSend,
+					ContentType: "audio/wav",
+				},
 			},
-		},
-	)
-	if err != nil {
-		log.Println(err)
-		w.Write([]byte(`<script>window.location="/";alert('Request sending error')</script>`))
-		return
-	}
+		)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
 
-	w.WriteHeader(200)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(Response{Text: msg.Text})
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(Response{Text: msg.Text})
+	}
 }
 
 func sever() {
